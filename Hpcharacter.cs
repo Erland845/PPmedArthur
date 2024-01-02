44faa5ae224dfa8ee8f9aa710c3e86db6e22398d
Erland845 <141270802+Erland845@users.noreply.github.com>
2024-01-02 14:54:18 +0100

    Add project files.

---
+++ Hpcharacter.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+
+public class Hpcharacter
+{
+	public string Name { get; set; }
+	public string House { get; set; }
+	public List<string> Inventory { get; set; }
+
+	public Hpcharacter (string name, string house, List <string> inventory)
+	{
+		Name = name;
+		House = house;
+		Inventory = inventory;
+
+	}
+	public void PrintInfo()
+	{
+		Console.WriteLine($"Denne karakteren heter {Name}, tilhører huset {House}, og har {PrintInventory()}");
+
+		
+
+	}
+	public string PrintInventory()
+	{
+		string inventory = "";
+		for (int i = 0; i < Inventory.Count; i++)
+		{
+			inventory += Inventory[i] + ", ";
+		}
+		return inventory;
+	}
+}
+public class Butikk
+{
+	public List<string> Items { get; set; }
+
+	public Butikk ( List<string> items)
+	{
+		Items = items;
+	}
+	public void PrintInfo() 
+	{
+		Console.WriteLine($"I denne butikken kan du skaffe deg: \n{PrintStore()}"); 
+
+	}
+	public string PrintStore()
+	{
+		string items = "";
+		for (int i = 0; i < Items.Count; i++)
+		{
+			items += $"{i + 1}. " + Items[i] + "\n";
+		}
+		return items;
+	}
+	
+		
+		
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace PPmedArthur
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hpcharacter Harry = new Hpcharacter("Harry", "Griffindor", new List<string> {"Tryllestav", "Nimbus2000", "briller"});
+            Harry.PrintInfo();
+
+            Butikk HPbutikk = new Butikk(new List<string> { "Ugle", "Rotte", "Katt", "Tryllestav" });
+
+
+            Console.WriteLine("Har du lyst til å kjøpe items til karakteren din?");
+            var input = Console.ReadLine(); 
+
+            if (input == "Ja")
+            {
+                HPbutikk.PrintInfo();
+            }
+            else
+            {
+                Console.WriteLine("Ok");
+            }
+        }
+
+    }
+}
